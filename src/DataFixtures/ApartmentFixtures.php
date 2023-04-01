549a8f86734a0d659c51a48b98282c3c6cf100df
Anissa Boukerche <117371334+AnissaBoukerche@users.noreply.github.com>
2023-04-01 13:56:11 +0200

    fix Apartment

--- src/DataFixtures/ApartmentFixtures.php
+++ src/DataFixtures/ApartmentFixtures.php
@@ -4,21 +4,34 @@ namespace App\DataFixtures;
 
 use App\Entity\Apartment;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class ApartmentFixtures extends Fixture
+class ApartmentFixtures extends Fixture implements DependentFixtureInterface
 {
+    const APARTMENT_REFERENCE = 'Apartment';
     public function load(ObjectManager $manager): void
     {
         $apartment = new Apartment;
+        $userAgency = $this->getReference(UserAgencyFixtures::USER_AGENCY_REFERENCE);
+
 
         $apartment->setAddress('10 rue de la Rose')
         ->setAdditionalAddressDetails('Appartment 2B')
         ->setCity('Marseille')
-        ->setZipCode('13000');
+        ->setZipCode('13000')
+        ->setUserAgency($userAgency);
         
+        $this->addReference(self::APARTMENT_REFERENCE, $apartment);
         $manager->persist($apartment);
 
         $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            UserAgencyFixtures::class,
+        ];
     }
 }
