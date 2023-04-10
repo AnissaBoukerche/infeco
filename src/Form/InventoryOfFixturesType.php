63385f9ee5d070834bc4eda20ddacde6c9a846fc
Anissa Boukerche <117371334+AnissaBoukerche@users.noreply.github.com>
2023-04-10 14:19:39 +0200

    fix InventoryOfFixtures

--- src/Form/InventoryOfFixturesType.php
+++ src/Form/InventoryOfFixturesType.php
@@ -3,7 +3,6 @@
 namespace App\Form;
 
 use App\Entity\InventoryOfFixtures;
-use App\Entity\Rental;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
